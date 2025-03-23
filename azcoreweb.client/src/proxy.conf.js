37b05190e85df6a955741e5be0b3c4bf0499993a
smthbh <smthbh@gmail.com>
2025-03-22 20:51:40 -0400

    add account creation to ui

--- azcoreweb.client/src/proxy.conf.js
+++ azcoreweb.client/src/proxy.conf.js
@@ -6,7 +6,8 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 const PROXY_CONFIG = [
   {
     context: [
-      "/status"
+      "/status",
+      "/Account/Create"
     ],
     target,
     secure: false
